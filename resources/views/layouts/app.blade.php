bd46ec2aa62b703727f04bd49f2827844eaffbea
Pavel Lipenkov <l.pav71@gmail.com>
2021-01-25 20:18:51 +0300

    Commit 1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <!-- Bootstrap CSS -->
     {{--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">--}}
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="/css/site.css">
+    <link href="{{ asset('css/site.css') }}" rel="stylesheet">
 
 
     <title>CRUD Full!!!</title>
