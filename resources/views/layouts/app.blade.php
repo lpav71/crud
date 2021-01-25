14538ee35a8328d447cc32213ec552e4f63a4dbd
Pavel Lipenkov <l.pav71@gmail.com>
2021-01-25 20:19:22 +0300

    Commit 1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&family=Russo+One&display=swap" rel="stylesheet">
 
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <!-- Bootstrap CSS -->
-    {{--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">--}}
+
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/site.css') }}" rel="stylesheet">
 
